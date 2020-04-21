0ea9ac07293f0a4a0c04538db97976da5abf4a84
JelleBootsma <jellehb@gmail.com>
2020-04-21 21:59:03 +0200

    Added materialize, removed bootstrap

--- js/main.js
+++ js/main.js
@@ -8,14 +8,23 @@ var launchesApp = new Vue({
     el: '#launchesApp',
     data: {
         launches: [
-        ]
+        ],
+        backgroundPicture: null
     },
     mounted: function (newLaunches) {
         this.updateLaunches(newLaunches);
     },
     methods:{
         updateLaunches: function (newLaunches) {
+            newLaunches.sort((a, b) => (a.launch_date_utc > b.launch_date_utc) ? 1 : -1)
             this.launches = newLaunches;
+            let pic;
+            for (pic in newLaunches) {
+                if (newLaunches[pic].links.flickr_images[0]){
+                    this.backgroundPicture = newLaunches[pic].links.flickr_images[0];
+                    break;
+                }
+            }
         }
     }
 })
