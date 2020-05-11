e0890d41acbb2741ccae34f21659390fdad68e5d
JelleBootsma <jellehb@gmail.com>
2020-05-11 08:36:31 +0200

    PWA

---
+++ sw.js
@@ -0,0 +1,32 @@
+﻿﻿
+self.addEventListener('install', (event) => {
+
+});
+
+self.addEventListener('activate', (event) => {
+    // console.log('👷', 'activate', event);
+    return self.clients.claim();
+});
+
+self.addEventListener('fetch', function (event) {
+    // console.log('👷', 'fetch', event);
+    if (event.request.method == "GET" && (
+            event.request.url.toLowerCase().endsWith('.jpg') ||
+            event.request.url.toLowerCase().endsWith('.jpeg') ||
+            event.request.url.toLowerCase().endsWith('.png')
+            )) {
+        event.respondWith(
+            caches.open('spacex-imageCache').then(function (cache) {
+                return cache.match(event.request).then(function (response) {
+                    return response || fetch(event.request).then(function (response) {
+                        cache.put(event.request, response.clone());
+                        return response;
+                    });
+                });
+            })
+        );
+    }
+    else {
+        event.respondWith(fetch(event.request));
+    }
+});
